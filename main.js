41b1b441a5ee32dec628e4bf21bff4229e94d2f4
Sebastian Real <seba.real.a@gmail.com>
2016-03-07 15:06:08 -0300

    new changes to app

---
+++ main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class LoginApp extends React.Component {
+  render(){
+    return <div>This is the login.</div>;
+  }
+}
+
+ReactDOM.render(<LoginApp />, document.getElementById('myLogin'));
