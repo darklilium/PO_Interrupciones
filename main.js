3a168d607f716db8d11ee5ea5e280c0a344d7238
Evelyn Hernández <dark.desires@live.cl>
2016-03-08 18:10:51 -0300

    Trying to ajax - login with errors

--- main.js
+++ main.js
@@ -2,15 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class LoginApp extends React.Component {
+  constructor(){
+    super();
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(){
+    var genericAccount = {
+        username: 'vialactea\\ehernanr',
+        password:'Chilquinta5',
+        client: 'requestip'
+      };
+    var userValue = this.refs.username.value;
+    var passValue = this.refs.password.value;
+
+    genericLogin(genericAccount, userValue, passValue);
+
+}
   render(){
     return (
       <div className="wrapper">
         <img className="logo" src="images/logo_gisred500x500.png" />
 
         <div className="login">
-          <input className="login__input" id="txtUsuario" type="text" />
-          <input className="login__input" id="txtPassword" type="text" />
-          <input className="login__submit" id="loginBtn" type="submit" />
+          <input className="login__input" ref="username" type="text" />
+          <input className="login__input" ref="password" type="password" />
+          <input className="login__submit" type="submit" onClick={this.onClick} />
         </div>
 
         <div className="footer">
@@ -25,3 +42,31 @@ class LoginApp extends React.Component {
 }
 
 ReactDOM.render(<LoginApp />, document.getElementById('myLogin'));
+
+
+function genericLogin(genericAccount, username, password){
+  console.log(username + " " + password + " "+ genericAccount.username + " " + genericAccount.password );
+  var url = "http://143.47.57.116/arcgis/tokens/";
+
+var jQuery = $.noConflict();
+jQuery.ajax({
+     type: 'GET',
+     url: url,
+     data: {
+       username: genericAccount.username,
+       password: genericAccount.password,
+       client: genericAccount.client,
+       format: 'json'
+     },
+     success: (success) =>{
+       console.log("here i need to call to a function for requesting service access");
+
+     },
+     error: (error) => {
+          console.log("sumthing happen ):");
+     }
+  });
+}
+
+
+//console.log(genericAccount.genericUser);
