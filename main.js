78db5042d6f0485e5d73135874d4539ecb350c6c
Evelyn Hernández <dark.desires@live.cl>
2016-03-08 12:34:07 -0300

    Adding Login elements

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ import ReactDOM from 'react-dom';
 
 class LoginApp extends React.Component {
   render(){
-    return <div>This is the login.</div>;
+    return <div className="Container">
+            <div className="Login">
+              <img id="imgLogin" src="images/logo_gisred500x500.png" width="150px" height="150px"></img>
+              <input id="txtUsuario" type="text"  />
+              <input id="txtPassword" type="text" />
+              <input id="LoginBtn" type="submit"/>
+            </div>
+
+             <div className="Footer">
+              <h4>Planificación y Gestión de la Información Operacional</h4>
+              <h6>Av. Argentina N°1 Piso 7</h6>
+             </div>
+          </div>;
   }
 }
 
