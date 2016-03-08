9fa74e7d0ae9baa684fd5c2e86bbec4b77d54ba6
Sebastian Real <seba.real.a@gmail.com>
2016-03-08 16:15:11 -0300

    adding some fixes to name conventions and just a couple of features added to CSS styles

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class LoginApp extends React.Component{
+class LoginApp extends React.Component {
   render(){
-    return <div className="wrapper">
-            <img className="wrapper__ImageLogo" src="images/logo_gisred500x500.png"></img>
-            <div className="wrapper__Login">
-              <input className="login__Inputs" id="txtUsuario" type="text"  />
-              <input className="login__Inputs" id="txtPassword" type="text" />
-              <input className="login__Submits" id="LoginBtn" type="submit"/>
-            </div>
+    return (
+      <div className="wrapper">
+        <img className="logo" src="images/logo_gisred500x500.png" />
 
-             <div className="wrapper__Footer">
-               <img className="wrapper__ImageChq" src="images/chq_i.png"></img>
-                <p className="wrapper__Footer__p">Planificación y Gestión</p><p className="wrapper__Footer__p">de la Información Operacional</p>
-                <h6 className="wrapper__Footer__h6">Av. Argentina N°1 Piso 7</h6>
-             </div>
-          </div>;
+        <div className="login">
+          <input className="login__input" id="txtUsuario" type="text" />
+          <input className="login__input" id="txtPassword" type="text" />
+          <input className="login__submit" id="loginBtn" type="submit" />
+        </div>
+
+        <div className="footer">
+          <img className="image-chq" src="images/chq_i.png" />
+          <p className="footer__p">Planificación y Gestión</p>
+          <p className="footer__p">de la Información Operacional</p>
+          <h6 className="footer__h6">Av. Argentina N°1 Piso 7</h6>
+        </div>
+      </div>
+    );
   }
 }
 
