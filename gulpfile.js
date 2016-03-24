1e4191ea691f15ec9be66e4a8b7f8fd84085f6d5
Sebastian Real <seba.real.a@gmail.com>
2016-03-24 12:29:29 -0300

    adding gulp task to allow sass compiling

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
 var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
-var notify = require('gulp-notify');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+//var notify = require('gulp-notify');
 
-function handleError(){
-  var args = [].slice.call(arguments);
-
-  notify.onError({
-    title: 'compile error',
-    message: '<%= error.message %>'
-  }).apply(this, args);
-}
+// function handleError(){
+//   var args = [].slice.call(arguments);
+//
+//   notify.onError({
+//     title: 'compile error',
+//     message: '<%= error.message %>'
+//   }).apply(this, args);
+// }
 
 function defaultError(type){
   return function(err){
@@ -24,51 +22,22 @@ function realPath(xs){
   return './static/' + xs;
 }
 
-var apps = [
-  //example
-  //{ input: 'js/apps/example_app.jsx', output:'example.bundle.js'}
-];
+var styles = [
+  'css/styles.scss',
+  'css/login.scss'
+].map(realPath);
 
-var styles = [].map(realPath);
-
-gulp.task('css', function(){
-  var g = gulp.src(styles)
-    .pipe(stylus({compress: true}))
-    .on('error', defaultError('stylus'))
-    .pipe(gulp.dest(realPath('css/')));
-
-  return g;
-});
-
-gulp.task('build', function(){
-  apps.forEach(function(opt){
-    var choices = {
-      entries: [realPath(opt.input)],
-      extensions: ['.js', '.jsx']
-    };
-
-    var presets = {presets: ['es2015', 'react']};
-
-    browserify(choices)
-      .transform(babelify.configure(presets))
-      .bundle()
-      .on('error', handleError)
-      .pipe(source(opt.output))
-      .pipe(gulp.dest(realPath('js/bundles')));
+gulp.task('sass', function(){
+  var handlers = styles.map(function(fileDir){
+    return gulp.src(fileDir)
+      .pipe(sass({ outputStyle: 'compact' }))
+      .on('error', defaultError)
+      .pipe(gulp.dest(realPath('css')));
   });
 });
 
 gulp.task('watch', function(){
-  gulp.watch([
-    'js/components/**/*',
-    'js/services/*',
-    'js/apps/*',
-    'js/actions/*',
-    'js/stores/*'
-  ].map(realPath), ['build']);
-
-  gulp.watch(['css/**/*.styl'].map(realPath), ['css']);
+  gulp.watch(['css/**/*.scss'].map(realPath), ['sass']);
 });
 
-
-gulp.task('default', ['css', 'build', 'watch']);
+gulp.task('default', ['sass', 'watch']);
