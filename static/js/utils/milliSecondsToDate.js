cd4713b802d83ad4e90f4eb3094c467f44e031fa
Evelyn Hernández <dark.desires@live.cl>
2016-04-15 10:56:02 -0300

    Fixed some issues with dates

--- static/js/utils/milliSecondsToDate.js
+++ static/js/utils/milliSecondsToDate.js
@@ -1,14 +1,29 @@
-function formatDate (date){
-  var d = new Date(date);
-  var str = "day/month/year, hour:minute:second"
-    .replace('day', d.getDate() <10? '0'+ d.getDate() : d.getDate())
-    .replace('month', d.getMonth()+1 <10? '0' + d.getMonth() : d.getMonth())
-    .replace('year', d.getFullYear())
-    .replace('hour', d.getHours() <10? '0'+ d.getHours() : d.getHours() )
-    .replace('minute', d.getMinutes() <10? '0'+ d.getMinutes() : d.getMinutes())
-    .replace('second', d.getSeconds() <10? '0'+ d.getSeconds() : d.getSeconds());
+function formatDate(badDate){
+      if(typeof badDate === 'number'){
+        badDate = new Date(badDate);
+      }
 
-  return str;
-}
+      var d = new Date();
+
+      d.setDate(badDate.getUTCDate());
+      d.setMonth(badDate.getUTCMonth());
+      d.setYear(badDate.getUTCFullYear());
+      d.setHours(badDate.getUTCHours());
+      d.setMinutes(badDate.getUTCMinutes());
+      d.setSeconds(badDate.getUTCSeconds());
+      d.setMilliseconds(badDate.getUTCMilliseconds());
+
+
+      var str = "day/month/year, hour:minute:second"
+        .replace('day', d.getDate() <10? '0'+ d.getDate() : d.getDate())
+        .replace('month', d.getMonth()+1 <10? '0' + d.getMonth() : d.getMonth())
+        .replace('year', d.getFullYear())
+        .replace('hour', d.getHours() <10? '0'+ d.getHours() : d.getHours() )
+        .replace('minute', d.getMinutes() <10? '0'+ d.getMinutes() : d.getMinutes())
+        .replace('second', d.getSeconds() <10? '0'+ d.getSeconds() : d.getSeconds());
+
+    return str;
+    }
+
 
 export default formatDate;
