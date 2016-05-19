f9c030866355715c3c38ee012c1bb984497ae59e
Evelyn Hernández <dark.desires@live.cl>
2016-05-19 15:49:26 -0400

    Some fixes on code compatibility

--- static/js/services/layers-service.js
+++ static/js/services/layers-service.js
@@ -10,37 +10,46 @@ function myLayers(){
   var graphicLayer = new esri.layers.GraphicsLayer();
 
   return {
-  //The following layers and services are just for Chilquinta APP. (interrupciones.html and interruptions.js)
-    //Featurelayer for orders per sed (with graphics)
+    //The following layers are for common use in any gisred app.
     read_tokenURL(){
       return serviceMain + "tokens/generateToken";
     },
+
+    read_logAccess(){  /*using*/
+        return serviceURL + "Admin/LogAccesos/FeatureServer/2?f=json&token=" + token.read();
+    },
+    //chq mapabase
     read_mapabase(){
       return serviceURL + "MapaBase/MapServer?f=json&token=" + token.read();
     },
+    //dmps adresses
     read_cartography(){
       return serviceURL + "Cartografia/DMPS/MapServer/0?f=json&token=" + token.read();
     },
+
+    //The following layers and services are just for Interruptions app. (interrupciones.html and interruptions.js)
+    //Featurelayer for orders per sed (with graphics)
+
     read_layer_interr_sed(){ /*using : Layer: SED*/
-      return serviceURL + "Interrupciones/PO/MapServer/0?f=json&token=" + token.read();
+      return serviceURL + "Interrupciones/PO_test/MapServer/0?f=json&token=" + token.read();
     },
     //Featurelayer for orders per customer (with graphics)
     read_layer_interr_clie(){ /*using: Layer Clientes*/
-      return serviceURL + "Interrupciones/PO/MapServer/1?f=json&token=" + token.read();
+      return serviceURL + "Interrupciones/PO_test/MapServer/1?f=json&token=" + token.read();
     },
     //Feature layer for BT: Red BT
     read_layer_tramosBT(){ /*using Layer Tramos*/
-      return serviceURL + "Interrupciones/PO/MapServer/2?f=json&token=" + token.read();
+      return serviceURL + "Interrupciones/PO_test/MapServer/2?f=json&token=" + token.read();
     },
     //Feature layer for getting the nis affected in the SED (green points)
     read_layer_ClienteSED(){ /*using Layer Cliente sed puntos verdes al extent*/
-      return serviceURL + "Interrupciones/PO/MapServer/3?f=json&token=" + token.read();
+      return serviceURL + "Interrupciones/PO_test/MapServer/3?f=json&token=" + token.read();
     },
     read_layer_CriticalCustomers(){ /*using Layer Cliente sed puntos verdes al extent*/
-      return serviceURL + "Interrupciones/PO/MapServer/4?f=json&token=" + token.read();
+      return serviceURL + "Interrupciones/PO_test/MapServer/4?f=json&token=" + token.read();
     },
     read_layer_CriticalPerSED(){ /*using Layer Cliente sed puntos verdes al extent*/
-      return serviceURL + "Interrupciones/PO/MapServer/5?f=json&token=" + token.read();
+      return serviceURL + "Interrupciones/PO_test/MapServer/5?f=json&token=" + token.read();
     },
     //Table for PO Orders (without graphics)
     read_layer_poOrdenes(){ /*using*/
@@ -48,10 +57,10 @@ function myLayers(){
     },
     //Feature layer for customers data  : Clientes (0)
     read_layer_ClieSED(){ /*using*/
-      return serviceURL + "Interrupciones/PO/MapServer/9?f=json&token=" + token.read();
+      return serviceURL + "Interrupciones/PO_test/MapServer/9?f=json&token=" + token.read();
     },
     read_layer_countTotal(){ /*using*/
-      return serviceURL + "Interrupciones/PO/MapServer/10?token=" + token.read();
+      return serviceURL + "Interrupciones/PO_test/MapServer/10?token=" + token.read();
     },
     read_layer_infoSED(){/*using for getting the sed information and location*/
       return serviceURL + "Chilquinta_006/Equipos_pto_006/MapServer/1?f=json&token=" + token.read();
@@ -66,16 +75,16 @@ function myLayers(){
       return graphicLayer;
     },
     read_dyn_layerClieSED(){  /*using*/
-        return serviceURL + "Interrupciones/PO/MapServer?f=json&token=" + token.read();
+        return serviceURL + "Interrupciones/PO_test/MapServer?f=json&token=" + token.read();
     },
     read_qtty_comuna(){  /*using*/
-        return serviceURL + "Interrupciones/PO/MapServer/11?f=json&token=" + token.read();
+        return serviceURL + "Interrupciones/PO_test/MapServer/11?f=json&token=" + token.read();
     },
     read_qtty_office(){  /*using*/
-        return serviceURL + "Interrupciones/PO/MapServer/12?f=json&token=" + token.read();
+        return serviceURL + "Interrupciones/PO_test/MapServer/12?f=json&token=" + token.read();
     },
     read_qtty_total_comuna(){  /*using*/
-        return serviceURL + "Interrupciones/PO/MapServer/13?f=json&token=" + token.read();
+        return serviceURL + "Interrupciones/PO_test/MapServer/13?f=json&token=" + token.read();
     },
     write_logAccess(){  /*using*/
         return serviceURL + "Admin/LogAccesos/FeatureServer/1/applyEdits?f=json&token=" + token.read();
@@ -83,6 +92,11 @@ function myLayers(){
 
     read_layerAlimentador(){  /*using*/
         return serviceURL + "Chilquinta_006/Tramos_006/MapServer?f=json&token=" + token.read();
+    },
+
+    //layers for AP CHQ
+    read_ap_comuna(){
+      return serviceURL + "AP_Municipal/AP_MUNICIPAL/MapServer/4?f=json&token=" + token.read();
     }
 
   };
@@ -129,6 +143,12 @@ function setLayers(){
       //interrClienteSED.refreshInterval = 1;
       cuadrillasLayer.setImageFormat("png32");
       return cuadrillasLayer;
+    },
+    ap_comuna(whereRegion, layerNumber){
+      var apComunaLayer = new esri.layers.FeatureLayer(myLayers().read_ap_comuna(),{id:"CHQAPComuna"});
+      apComunaLayer.setDefinitionExpression(whereRegion);
+      console.log(whereRegion);
+      return apComunaLayer;
     }
   }
 }
@@ -179,14 +199,13 @@ function addCertainLayer(layerNameToAdd, order, where){
 
   mapp.addLayer(myLayerToAdd);
 
-  //Set here if you add more layers in the layerlist. 
+  //Set here if you add more layers in the layerlist.
   if (check_alimentador.checked){
     mapp.addLayer(setLayers().alimentadores(),1);
   }
   if (check_ap_modificaciones.checked){
     //mapp.addLayer(setLayers().check_ap_modificaciones(), 1);
   }
-
 
 }
 
